7186e97b0e3a600fc667430e7eaf6b6858bd87e2
Andrei <andrei.martjanov2014@yandex.ru>
2022-02-02 22:51:02 +0300

    Sounds and Mode

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,58 +7,84 @@
       <div class="box greenback" @click="clk(3)" :class="{activegreen : activeClass == 3}"></div>
       <div class="box yellowback" @click="clk(4)" :class="{activeyellow : activeClass == 4}"></div>
     </div>
+    <div v-if="arr.length">
+      Уровень: {{arr.length}}
+    </div>
     <!-- <div class="game__start-btn" @click="startGame">{{ centerButton }}
         <span v-show="playing">{{ showScore }}</span>
     </div> -->
     <div>
       <button @click="Start">СТАРТ</button>
     </div>
+    <div style="margin-top:5px">
+      <button @click="Restart">Рестарт</button>
+    </div>
     <div class="game__mode">
-      <p>Mode</p>
-      <button :class="{active: activeBtn === 'btn1'}" @click="gameMode = 'easy'; activeBtn = 'btn1'">Easy</button>
-      <button :class="{active: activeBtn === 'btn2'}" @click="gameMode = 'normal'; activeBtn = 'btn2'">Normal</button>
-      <button :class="{active: activeBtn === 'btn3'}" @click="gameMode = 'hard'; activeBtn = 'btn3'">Hard</button>
+      <p>Режим</p>
+      <button :class="{active: activeBtn === 'btn1'}" @click="gameMode('easy'); activeBtn = 'btn1'">Легко</button>
+      <button :class="{active: activeBtn === 'btn2'}" @click="gameMode('normal'); activeBtn = 'btn2'">Нормально</button>
+      <button :class="{active: activeBtn === 'btn3'}" @click="gameMode('hard'); activeBtn = 'btn3'">Тяжело</button>
   </div>
   </div>
 </template>
 
 <script>
+import bum1 from "../assets/sounds/bum1.mp3"
+import bum2 from "../assets/sounds/bum2.mp3"
+import bum3 from "../assets/sounds/bum3.mp3"
+import bum4 from "../assets/sounds/bum4.mp3"
+import error from "../assets/sounds/error.mp3"
+import aplodis from "../assets/sounds/aplodis.mp3"
 export default {
+  // components:{
+  //   "bum1":bum1
+  // },
   name: 'SimonGame',
   data() {
     return {
       activeClass: null,
-      arr: [1,3,2,3],
+      arr: [],
       counter: 0,
-      record: null,
       chek:0,
       select:null,
-
-      centerButton: "START",
-      playing: false,
-      isClickable: false,
-      isWon: false,
-      isWrong: false,
-      gameMode: 'normal',
+      // gameMode: 'normal',
       activeBtn: 'btn2',
-      score: 0,
-      sequence: [],
-      sequenceInterval: null,
-      playerSequence: [],
+      mode: 1000,
+      speed:500,
       sounds: {
-        1: "sounds/plip.mp3",
-        2: "sounds/govilive.mp3",
-        3: "sounds/error.mp3",
-        4: "sounds/sswitch1.mp3",
-        5: "sounds/haa.mp3",
-        6: "sounds/good.mp3",
+        1: bum1,
+        2: bum2,
+        3: bum3,
+        4: bum4,
+        5: error,
+        6: aplodis,
       },
-      isLit: {
-        1: false,
-        2: false,
-        3: false,
-        4: false
-      }
+
+      // centerButton: "START",
+      // playing: false,
+      // isClickable: false,
+      // isWon: false,
+      // isWrong: false,
+      // // gameMode: 'normal',
+      // // activeBtn: 'btn2',
+      // score: 0,
+      // sequence: [],
+      // sequenceInterval: null,
+      // playerSequence: [],
+      // sounds: {
+      //   1: "sounds/plip.mp3",
+      //   2: "sounds/govilive.mp3",
+      //   3: "sounds/error.mp3",
+      //   4: "sounds/sswitch1.mp3",
+      //   5: "sounds/haa.mp3",
+      //   6: "sounds/good.mp3",
+      // },
+      // isLit: {
+      //   1: false,
+      //   2: false,
+      //   3: false,
+      //   4: false
+      // }
     }
   },
   // computed: {
@@ -70,13 +96,46 @@ export default {
   //   }
   // },
   methods: {
+    Restart(){
+      this.chek=0
+      this.arr=[]
+      this.counter=0
+      this.select=null
+      this.activeClass = null
+
+    },
+    gameMode(req){
+      if (req=='easy'){
+        this.mode=1500
+        this.speed=700
+      }
+      else if (req=='normal'){
+        this.mode=1000
+        this.speed=500
+      }
+      else{
+        this.mode=400
+        this.speed=300
+      }
+    },
     clk(data){
       this.activeClass = data
       this.select = data
       this.checkWinLose(data);
+      var audio = new Audio(this.sounds[data]);
+      audio.play();
       this.pool = setTimeout(() => {
         this.activeClass = null
+      },250)
+    },
+    adt(){
+      this.pool = setTimeout(() => {
+        alert('Победа')
+        var audio = new Audio(this.sounds[6]);
+        audio.play();
+        this.Restart()
       },700)
+      
     },
     checkWinLose(){
       for(this.chek; this.chek <= this.arr.length;){
@@ -84,60 +143,77 @@ export default {
         if (this.select == this.arr[this.chek]){
           console.log('успех')
           if(this.chek+1 == this.arr.length){
-            console.log('win')
-            break
+            console.log('win'+ this.chek)
+            if(this.chek == 5){
+              this.adt()
+              break
+            }
+            else{
+              this.Start()
+            }
           }
           this.chek++
           break
         }
         else{
-          console.log("промах")
-          this.chek=0
+          var audio = new Audio(this.sounds[5]);
+          audio.play();
+          alert('Вы проиграли')
+          this.Restart()
           break
         }
       }
     },
-    lol(){
+    test(){
         this.activeClass = this.arr[this.counter]
         this.counter++
+        var audio = new Audio(this.sounds[this.activeClass]);
+        audio.play();
+        this.pool = setTimeout(() => {
+          this.activeClass=null
+      },this.speed)
+        
     },
     Start(){
+      this.arr.push(Math.floor(Math.random() * (4 - 1+ 1 ) + 1))
       this.polling = setInterval(() => {
-        this.lol()
+        
+        this.test()
         if (this.arr.length+1 == this.counter){
           clearInterval(this.polling)
           this.counter=0
           this.activeClass=null
+          this.chek=0
         }
-      },500)
+      },this.mode)
       
-    },
-    playSound(tile) {
-      if(this.sounds[tile]) {
-        var audio = new Audio(this.sounds[tile]);
-        audio.play();
-      }
-    },
-    startGame() {
-      this.playing = true;
-      this.sequence = [];
-      this.playSound(5);
-      this.playerSequence = [];
-      this.centerButton = "RESET";
-      this.isWon = false;
-      this.isWrong = false;
-      this.score = 0;
-      clearInterval(this.sequenceInterval);
-      this.showSequence();
-    },
-    clicked(tile) {
-      if (this.isClickable) {
-        this.playSound(tile);
-        this.lightUp(tile);
-        this.playerSequence.push(tile);
-        this.checkWinLose();
-      }
-    },
+    }
+    // playSound(tile) {
+    //   if(this.sounds[tile]) {
+    //     var audio = new Audio(this.sounds[tile]);
+    //     audio.play();
+    //   }
+    // },
+    // startGame() {
+    //   this.playing = true;
+    //   this.sequence = [];
+    //   this.playSound(5);
+    //   this.playerSequence = [];
+    //   this.centerButton = "RESET";
+    //   this.isWon = false;
+    //   this.isWrong = false;
+    //   this.score = 0;
+    //   clearInterval(this.sequenceInterval);
+    //   this.showSequence();
+    // },
+    // clicked(tile) {
+    //   if (this.isClickable) {
+    //     this.playSound(tile);
+    //     this.lightUp(tile);
+    //     this.playerSequence.push(tile);
+    //     this.checkWinLose();
+    //   }
+    // },
     // checkWinLose() {
     //   // check for incorrect
     //   for (let i = 0; i < this.playerSequence.length; i++) {
@@ -167,41 +243,41 @@ export default {
     //     }
     //   }
     // },
-    lightUp(tile) {
-      this.isLit[tile] = true;
-      setTimeout(() => {
-        this.isLit[tile] = false;
-      }, 300);
-    },
-    showSequence(redo) {
-      let currentIndex = 0;
-      let speed = this.sequence.length === 0 ? 1000 : this.getModeSpeed(this.gameMode);
-      this.isClickable = false;
-      if (!redo) {
-        // dont add number on incorrect answers
-        this.sequence.push(Math.floor(Math.random() * 4 + 1));
-      }
-      this.sequenceInterval = setInterval(() => {
-        if (currentIndex >= this.sequence.length) {
-          clearInterval(this.sequenceInterval);
-          return (this.isClickable = true);
-        }
-        this.playSound(this.sequence[currentIndex]);
-        this.lightUp(this.sequence[currentIndex]);
-        currentIndex++;
-      }, speed);
-    },
-    getModeSpeed(mode) {
-      if (mode === 'easy') {
-        return 1500;
-      }
-      else if (mode === 'normal') {
-        return 1000;
-      }
-      else {
-        return 400;
-      }
-    }
+    // lightUp(tile) {
+    //   this.isLit[tile] = true;
+    //   setTimeout(() => {
+    //     this.isLit[tile] = false;
+    //   }, 300);
+    // },
+    // showSequence(redo) {
+    //   let currentIndex = 0;
+    //   let speed = this.sequence.length === 0 ? 1000 : this.getModeSpeed(this.gameMode);
+    //   this.isClickable = false;
+    //   if (!redo) {
+    //     // dont add number on incorrect answers
+    //     this.sequence.push(Math.floor(Math.random() * 4 + 1));
+    //   }
+    //   this.sequenceInterval = setInterval(() => {
+    //     if (currentIndex >= this.sequence.length) {
+    //       clearInterval(this.sequenceInterval);
+    //       return (this.isClickable = true);
+    //     }
+    //     this.playSound(this.sequence[currentIndex]);
+    //     this.lightUp(this.sequence[currentIndex]);
+    //     currentIndex++;
+    //   }, speed);
+    // },
+    // getModeSpeed(mode) {
+    //   if (mode === 'easy') {
+    //     return 1500;
+    //   }
+    //   else if (mode === 'normal') {
+    //     return 1000;
+    //   }
+    //   else {
+    //     return 400;
+    //   }
+    // }
   }
 }
 </script>
